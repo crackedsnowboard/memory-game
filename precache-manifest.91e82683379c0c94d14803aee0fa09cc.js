44418a2159575bc1b5c5b45d36e3d76d734b465b
Prince of Kochi <mathenjs@gmail.com>
2020-08-11 19:01:21 -0700

    Updates

---
+++ precache-manifest.91e82683379c0c94d14803aee0fa09cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8307eca5a986fef7fcc6c97d5a4501b4",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "d8134ac2b602c0e099b8",
+    "url": "/memory-game/static/css/main.54bdcd99.chunk.css"
+  },
+  {
+    "revision": "f9e4f94c64f02375af1f",
+    "url": "/memory-game/static/js/2.6de7d4d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.6de7d4d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8134ac2b602c0e099b8",
+    "url": "/memory-game/static/js/main.bf96e2bf.chunk.js"
+  },
+  {
+    "revision": "8f756061ff4c5bff5cbb",
+    "url": "/memory-game/static/js/runtime-main.2f259375.js"
+  }
+]);
